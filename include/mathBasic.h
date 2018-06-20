d01cd5543c752c5740c7ce5a391c41b8975d22b5
Eric Andrade <vloxflox@gmail.com>
2018-06-20 19:49:38 -0300

    adicionei o prototipo do mathbasic

---
+++ include/mathBasic.h
@@ -0,0 +1,14 @@
+#ifndef MATHBASIC_H
+#define MATHBASIC_H
+
+class MathBasic
+{
+    public:
+    static int add(int,int);
+    static int dif(int,int);
+    static int mux(int,int);
+    static int div(int,int);
+};
+
+
+#endif // MATHBASIC_H
