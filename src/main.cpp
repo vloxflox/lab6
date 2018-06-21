1525a1f5a0404d932a5dd9d4210e3d988cda5cc8
Eric Andrade <vloxflox@gmail.com>
2018-06-21 12:25:03 -0300

    refiz as partes que foram apagadas

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "../include/mathBasic.h"
 
 int main(){
+	int op1 = 4;
+	int op2 = 5;
+	std::cout<<"Operandos "<< op1 <<" e "<< op2 <<std::endl; 
+	std::cout<<"SOMANDO: ";
+	std::cout<<mbasic::MathBasic::add(op1,op2)<<std::endl;
+	std::cout<<"SUBTRAINDO: ";
+	std::cout<<mbasic::MathBasic::dif(op1,op2)<<std::endl;
+	std::cout<<"MULTIPLICANDO: ";
+	std::cout<<mbasic::MathBasic::mux(op1,op2)<<std::endl;
+	std::cout<<"DIVISÃO INTEIRA: ";
+	std::cout<<mbasic::MathBasic::div(op1,op2)<<std::endl;
 
-	std::cout<<"helloWorld"<<std::endl;
-	
     return 0;
 }
---
+++ src/mathBasic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/mathBasic.h"
+
+int mbasic::MathBasic::add(int a,int b){return a+b;}
+int mbasic::MathBasic::dif(int a,int b){return a-b;}
+int mbasic::MathBasic::mux(int a,int b){return a*b;}
+int mbasic::MathBasic::div(int a,int b){return a/b;}
